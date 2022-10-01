895780b9e1831e22e5ab83bdbdbda4ad1a823af0
Channel Gonzalez Jimenez <channelgonzalezjimenez100@gmail.com>
2022-09-30 23:31:08 -0500

    sec1Vid12_cambiarAnimaciones

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,18 +8,22 @@ public class PlayerController : MonoBehaviour
 //variables movimiento 
 public float jumpForce = 6f;
 private Rigidbody2D rigidBody;
-
+Animator animator;
+const string STATE_ALIVE="isAlive";
+const string STATE_ON_THE_GROUD="isOnTheGround";
 
     public LayerMask groundMask;
 
     void Awake(){
         rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator.SetBool(STATE_ALIVE, true);
+        animator.SetBool(STATE_ON_THE_GROUD, true);
     }
 
     // Update is called once per frame
@@ -28,7 +32,8 @@ private Rigidbody2D rigidBody;
         if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
             Jump();
         }        
-    Debug.DrawRay(this.transform.position, Vector2.down*1.5f, Color.red);
+        animator.SetBool(STATE_ON_THE_GROUD, IsTouchingTheGround());
+        Debug.DrawRay(this.transform.position, Vector2.down*1.5f, Color.red);
     }
 
     void Jump(){
@@ -41,8 +46,11 @@ private Rigidbody2D rigidBody;
                                             Vector2.down,
                                             1.5f,
                                             groundMask)){
+                                                animator.enabled=true;
                                                 return true;
-                                            }else{return false;
+                                            }else{
+                                                animator.enabled=false;
+                                                return false;
                                             }
     }
 }
