baeb5bd4e0d446fb646783b520ab860358f3db89
Channel Gonzalez Jimenez <channelgonzalezjimenez100@gmail.com>
2022-10-26 00:29:53 -0500

    5_4_EnemigosMovilesPreparGiro

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -21,9 +21,18 @@ public class Enemy : MonoBehaviour
         this.transform.position = startPosition;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void FixedUpdate(){
+        float currentRunningSpeed = runningSpeed;
+        if(facingRight){
+            currentRunningSpeed = runningSpeed;
+            this.transform.eulerAngles = new Vector3(0, 180, 0);
+        }else{
+            currentRunningSpeed = -runningSpeed;
+            this.transform.eulerAngles = Vector3.zero;
+        }
+
+        if(GameManager.sharedInstance.currentGameState == GameState.inGame){
+            rigidBody.velocity = new Vector2(currentRunningSpeed, rigidBody.velocity.y);
+        }
     }
 }
